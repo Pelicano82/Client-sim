e67be1cfea4a921e327162fad1d0ef9818710d31
Pelicano82 <dkane1708@gmail.com>
2026-03-09 15:54:15 +0000

    Add files via upload

---
+++ clientSim/src/test/java/com/example/clientSim/ClientSimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientSim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
