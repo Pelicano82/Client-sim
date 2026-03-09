af34adec800b2f305ba8821ee6cff0903f0e0011
Pelicano82 <dkane1708@gmail.com>
2026-03-09 16:15:09 +0000

    Add files via upload

---
+++ clientSimTemplate/src/main/java/com/example/clientSimTemplate/ClientSimTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientSimTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSimTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSimTemplateApplication.class, args);
+	}
+
+}
---
+++ clientSimTemplate/src/test/java/com/example/clientSimTemplate/ClientSimTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientSimTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSimTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
