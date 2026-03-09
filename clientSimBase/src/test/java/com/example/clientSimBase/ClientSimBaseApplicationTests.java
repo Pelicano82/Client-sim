7231c86c15c455d7a9e36c12e973797c1c26bd72
Pelicano82 <dkane1708@gmail.com>
2026-03-09 16:00:40 +0000

    Add files via upload
    Don't edit this file, this is a backup

---
+++ clientSimBase/src/test/java/com/example/clientSimBase/ClientSimBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientSimBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSimBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
